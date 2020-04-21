63a55c3eff1be695ac44246572259bdee7ba4ade
Eujine <sponsor85@mail.ru>
2020-04-21 21:02:18 +0200

    Update PhoneBook.py
    fix acording to review

--- 03_HomeWork/PhoneBook.py
+++ 03_HomeWork/PhoneBook.py
@@ -1,18 +1,25 @@
 import sys
-help = "- to add new user type C;\n- to display existsing user type R;\n- to update existing user type U;\n- to remove existsing user type R;\n- to print help type H;\n- to terimate type Q"
+help ="""
+- to add new contact type C;
+- to display existsing contact type R;
+- to update existing contact type U;
+- to remove existsing contact type R;
+- to print help type H;
+- to terimate type Q
+"""
 
 print(help)
 
 phoneBook = {}
 
 while True:
-    commandType = input("Enter command : ")
+    commandType = input("Enter command : ").upper
     if commandType == "C" :
-        print("Add new user.")
+        print("Add new contact.")
         name = input("Enter contact name : ")
         phone = input("Enter contact phone : ")
         phoneBook[name] = phone
-        print(phoneBook)
+        #print(phoneBook)
     elif commandType == "R":
         print("Display phone.")
         name = input("Enter contact name : ")
@@ -20,38 +27,38 @@ while True:
             print(phoneBook[name])
         except KeyError:
             print("Not found")
-        except:
-            print("Error while delete user." + sys.exc_info()[0])
-        print(phoneBook)
+        except Exception as e:
+            print(f"Error while delete contact. {e}")
+        #print(phoneBook)
     elif commandType == "U":
-        print("Update existing user.")
+        print("Update existing contact.")
         name = input("Enter contact name : ")
         phone = input("Enter contact phone : ")
         try:
             phoneBook[name] = phone
         except KeyError:
             if phone in phoneBook.items: 
-                print("Name not found but phone already exists. Please delete coresponding user.")
+                print("Name not found but phone already exists. Please delete coresponding contact.")
                 print(phoneBook)
             else:
                 print("Not found")
-        except:
-            print("Error while delete user." + sys.exc_info()[0])            
-        print(phoneBook)
+        except Exception as e:
+            print(f"Error while delete contact. {e}")         
+        #print(phoneBook)
     elif commandType == "D":
-        print("Delete existing user.")
+        print("Delete existing contact.")
         name = input("Enter contact name : ")
         try:
             phoneBook.pop(name)
         except KeyError:
-            print("Not found")
-        except:
-            print("Error while delete user." + sys.exc_info()[0])
-        print(phoneBook)
+            print("Not found.")
+        except Exception as e:
+            print(f"Error while delete contact. {e}")
+        #print(phoneBook)
     elif commandType == "H":     
         print(help)
     elif commandType == "Q":
-        print("Program terminated")
+        print("Program terminated.")
         break
     else:
-        print("Unknown command")
+        print("Unknown command.")
